2587abf6fcc628a72af1f815f5c700bf5d2fc6d1
Yan-castro <yancastrostm@gmail.com>
2023-06-16 13:49:00 -0300

    attributes added to the task migration

--- db/migrate/20230616155913_create_tasks.rb
+++ db/migrate/20230616155913_create_tasks.rb
@@ -1,9 +1,9 @@
 class CreateTasks < ActiveRecord::Migration[7.0]
   def change
     create_table :tasks do |t|
-      t.string :title
+      t.string :title , null: false, limit: 255
       t.string :description
-      t.boolean :done
+      t.boolean :done, default: false, null: false
 
       t.timestamps
     end
